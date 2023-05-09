5ac1ab600991a64579d115208b6e2164868d1a14
Teun Schutten <132267480+TeunSchutten@users.noreply.github.com>
2023-05-09 12:53:08 +0200

    Footstep Joystick Input & Geometry and Texture Fiddle

--- Assets/Scripts/Footstep.cs
+++ Assets/Scripts/Footstep.cs
@@ -3,28 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace FSR {
-public class Footstep : MonoBehaviour
-{   
-    public FSR_Player fsr;
-    float elapsedTime;
-    float timeLimit = 0.5f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    public class Footstep : MonoBehaviour {
+        public FSR_Player fsr;
+        float elapsedTime;
+        float timeLimit = 0.5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-    elapsedTime += Time.deltaTime;
+        // Start is called before the first frame update
+        void Start() {
+        }
 
-    if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)){
-    if (elapsedTime >= timeLimit){
-        elapsedTime = 0;
-        fsr.step();
+        // Update is called once per frame
+        void Update() {
+            elapsedTime += Time.deltaTime;
+
+            // Handle input from WASD keys
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) {
+                if (elapsedTime >= timeLimit) {
+                    elapsedTime = 0;
+                    fsr.step();
+                }
+            }
+
+            // Handle input from Xbox Wireless Controller joystick
+            float joystickHorizontalInput = Input.GetAxis("Horizontal");
+            float joystickVerticalInput = Input.GetAxis("Vertical");
+            if (Mathf.Abs(joystickHorizontalInput) > 0 || Mathf.Abs(joystickVerticalInput) > 0) {
+                if (elapsedTime >= timeLimit) {
+                    elapsedTime = 0;
+                    fsr.step();
+                }
+            }
+        }
     }
-   }
-  }
- }
 }
